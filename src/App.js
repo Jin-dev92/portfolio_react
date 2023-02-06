776b7e988755da526f65be65965737aa4e8011b6
김의진 <camel@lizhko.io>
2023-02-06 13:55:15 +0900

    add mock data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import {Layout} from "antd";
 import IconButton from "./components/common/IconButton";
+import appData from './mock/app_data.json'
+
 function App() {
     const layoutStyle = {
         width: window.innerWidth,
         height: window.innerHeight,
         background: `url(/images/wallpaper/wallpaperbetter.com_1920x1200.jpg)`
     }
+
+
+    // console.table(appData)
     return (
         <Layout style={layoutStyle}>
-            <IconButton src={'/images/icons/computer/computer.png'}></IconButton>
+            {
+                appData.app_list.map((data, idx) => {
+                    return (
+                        <IconButton src={data.src} position={data.position} key={`icon-button-${idx}`}/>
+                    )
+                })
+            }
         </Layout>
     );
 }
