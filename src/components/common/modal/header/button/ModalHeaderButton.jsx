425a448322627d77b8e6ae9a9652e02556d96cdd
김의진 <camel@lizhko.io>
2023-02-10 16:51:38 +0900

    revoke

--- src/components/common/modal/header/button/ModalHeaderButton.jsx
+++
@@ -1,9 +0,0 @@
-import {Button} from "antd";
-
-const ModalHeaderButton = ({color, clickFunction}) => {
-    return (
-        <Button onClick={clickFunction} shape={"round"} ghost style={{backgroundColor: color}} size={"small"}></Button>
-    )
-}
-
-export default ModalHeaderButton
