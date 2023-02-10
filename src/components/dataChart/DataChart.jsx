df537f35262031bc024589b524eb2373a7ecce28
Jin-devST <kpasd002@gmail.com>
2023-02-10 18:17:15 +0900

    data fetch

--- src/components/dataChart/DataChart.jsx
+++ src/components/dataChart/DataChart.jsx
@@ -1,28 +1,27 @@
 import {useQuery} from "react-query";
 import {weatherServiceAPI} from "./api/api";
+import dayjs from "dayjs";
 
 const DataChart = () => {
     const config = {
         params: {
-            serviceKey: "avP3IfTpxDK50GQaRfRJf5iZHTFbFLkQHRkYCmphmqGAMs%2FToAKLbEdkn97qZzX4nd%2F2V5Tv%2FOwL%2BFVvR9Qw9g%3D%3D",
+            serviceKey: "avP3IfTpxDK50GQaRfRJf5iZHTFbFLkQHRkYCmphmqGAMs/ToAKLbEdkn97qZzX4nd/2V5Tv/OwL+FVvR9Qw9g==",
             pageNo: "1",
             numOfRows: "1000",
-            // dataType: "JSON",
-            base_date: "20210628",
-            base_time: "0500",
-            nx: "55",
+            dataType: "JSON",
+            base_date: dayjs().format("YYYYMMDD"),
+            base_time: dayjs().clone().subtract(1,"hours").format("HH").concat("00"),
+            nx: "60",
             ny: "127",
         },
-        headers: {
-            withCredentials: true,
-            'Content-Type': 'application/json',
-        }
     }
-
-    const {data, error, isLoading} = useQuery("weather", () => weatherServiceAPI(config), {
+    const {isLoading} = useQuery("weather", () => weatherServiceAPI(config), {
         retry: false,
-        onSuccess: (response) => {
-            console.log(response)
+        onSuccess: (data) => {
+            const {response} = data.data
+            // const {header} = response
+            // const {resultCode, resultMsg} = header
+            console.table(response.body.items)
         },
         onError: (error) => {
 
