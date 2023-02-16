bc9ee28a13432314d9cf223a55946bebe48494a8
Jin-devST <kpasd002@gmail.com>
2023-02-16 14:58:44 +0900

    외부 링크 연결

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import {Anchor, Divider} from "antd";
+import {Anchor, Divider, Dropdown, Space} from "antd";
 import Notification from "../common/notification/Notification";
-import {GithubOutlined} from "@ant-design/icons";
 import {NotificationType} from "../common/notification/constant";
 
 const About = () => {
@@ -21,6 +20,24 @@ const About = () => {
         currentAnchor.current = link
         setIsOpen(true)
     }
+    const items = [
+        {
+            key: '1',
+            label: (
+                <a target="_blank" rel="noopener noreferrer"
+                   href="https://daffy-bacon-3c6.notion.site/0c59987b2f604387a62328484bb2864b?v=58a9a33d63764677aca16b15d3f70954&p=a40d27f7b71745918a4c2f08b1eda6c6&pm=c">LG
+                    U+ 홈트Now STB</a>
+            )
+        },
+        {
+            key: '2',
+            label: (
+                <a target="_blank" rel="noopener noreferrer"
+                   href="https://daffy-bacon-3c6.notion.site/0c59987b2f604387a62328484bb2864b?v=58a9a33d63764677aca16b15d3f70954&p=f5c6cad1db7b4b75ab4339b554a9077a&pm=c">T-Map
+                    주차</a>
+            )
+        }
+    ]
 
     return (
         <div style={innerTextStyle}>
@@ -36,8 +53,33 @@ const About = () => {
                 <li>Javascript, ES6+ 표준 스펙을 활용한 UI 개발 경험</li>
                 <li>REST API를 활용한 WEB 개발 경험</li>
                 <li>HTML5, CSS3 기본 지식 및 마크업 능력, 반응형 웹 개발</li>
-                <li>자바스크립트 브릿지를 사용한 하이브리드 React 앱 제작 경험 (STB, AOS, IOS)</li>
-                <li>Google Analytics 의 데이터를 이용한 데이터 시각화 경험</li>
+                <li>
+                    자바스크립트 브릿지를 사용한
+                    <Dropdown
+                        menu={{items}}
+                        placement="bottomLeft"
+                        arrow
+                        trigger={'click'}
+                    >
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                        <a href={'#'} onClick={(event) => event.preventDefault()}>
+                            <Space>Hybrid React 앱</Space>
+                        </a>
+                    </Dropdown>
+                    제작 경험 (STB, AOS, IOS)
+                </li>
+                <li>Google Analytics 의
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a href={'https://ga-dev-tools.google/'}>
+                        <Space>데이터 패치 개발 도구</Space>
+                    </a>
+                    를 이용한
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a href={'https://daffy-bacon-3c6.notion.site/0c59987b2f604387a62328484bb2864b?v=58a9a33d63764677aca16b15d3f70954&p=c789d031b1d846ed9335979c75f7a915&pm=c'}>
+                        <Space>데이터 시각화</Space>
+                    </a>
+                    경험
+                </li>
                 <li>React 상태 관리 라이브러리 에 관한 이해 및 경험</li>
                 <li>atomic design 에 대한 이해</li>
             </ul>
@@ -78,7 +120,10 @@ const About = () => {
                           href={currentAnchor.current?.href}
                           type={NotificationType.INFO}
                           message={'해당 페이지로 이동 하시겠습니까?'}
-                          callback={window.location.href = currentAnchor.current?.href}/>
+                // callback={(event)=> {
+                //     window.location.href = currentAnchor.current?.href
+                // }}
+            />
         </div>
     )
 }
