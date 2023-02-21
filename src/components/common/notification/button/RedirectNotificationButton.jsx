5086f129eb1d587b9ff4702af1808d4fa08bfa32
Jin-devST <kpasd002@gmail.com>
2023-02-21 20:33:14 +0900

    마무리

--- src/components/common/notification/button/RedirectNotificationButton.jsx
+++ src/components/common/notification/button/RedirectNotificationButton.jsx
@@ -1,16 +1,22 @@
 import {Button, Space} from "antd";
 import React from "react";
+
 const RedirectNotificationButton = ({api, nKey, setIsOpen, href}) => {
     const confirmButtonHandler = async () => {
         api.destroy(nKey)
         setIsOpen(false)
     }
+    const cancelButtonHandler = () => {
+        api.destroy()
+        setIsOpen(false)
+    }
     return (
         <Space>
-            <Button type="link" size="small" onClick={() => api.destroy()}>
+            <Button type="link" size="small" onClick={cancelButtonHandler}>
                 취소
             </Button>
-            <Button type="primary" size="small" onClick={() => confirmButtonHandler().then(() => window.location.href = href)}>
+            <Button type="primary" size="small"
+                    onClick={() => confirmButtonHandler().then(() => window.location.href = href)}>
                 이동
             </Button>
         </Space>
