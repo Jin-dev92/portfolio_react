b0a60ebe0ae78003931a8e51ec946de8d0cb5327
Jin-devST <kpasd002@gmail.com>
2023-03-08 18:12:25 +0900

    list 변경

--- src/components/common/list/List.jsx
+++ src/components/common/list/List.jsx
@@ -4,7 +4,12 @@ export const List = styled.ul`
   list-style: none;
   padding-inline-start: 10px;
 
+  & > li {
+    float: ${props => props?.itemAlign === 'row' ? 'left' : undefined};
+  }
+
   & > li:not(:last-child) {
-    margin-inline-end: 10px;
+    margin-inline-end: ${props => props?.itemAlign === 'row' ? `${props.marginEnd}` : undefined};
+    margin-block-end: ${props => props?.itemAlign === 'col' ? `${props.marginEnd}` : undefined};
   }
 `
