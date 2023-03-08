7d4790e9006d70441ee7f08ed6d5fe465c47d72d
Jin-devST <kpasd002@gmail.com>
2023-03-08 17:31:10 +0900

    list 추가

--- src/components/chrome/Chrome.tsx
+++ src/components/chrome/Chrome.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import {Container} from "../common/layout/Container";
+import {List} from "../common/list/List";
+import {ListItem} from "../common/list/ListItem";
 
 const Chrome = () => {
     return (
         <Container>
-
+            <List>
+                <ListItem>1</ListItem>
+                <ListItem>1</ListItem>
+                <ListItem>1</ListItem>
+                <ListItem>1</ListItem>
+            </List>
         </Container>
     )
 }
