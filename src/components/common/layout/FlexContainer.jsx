bed2216292317c0d47a8a80fc84300cd36a4c6c1
Jin-devST <kpasd002@gmail.com>
2023-03-23 19:01:16 +0900

    타입스크립트 제거

---
+++ src/components/common/layout/FlexContainer.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+// import {Property} from "csstype";
+
+
+// type ContainerProps =  {
+//     direction: Property.Direction | undefined;
+// }
+
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: ${(props) => props?.direction || 'column'};
+  @media screen and (max-width: 350px) {
+    flex-direction: column;
+  }
+`;
