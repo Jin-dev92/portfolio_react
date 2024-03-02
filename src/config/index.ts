ea2d23e11087954d70e955b05b63ca99d57b5375
Jin-devST <kpasd002@gmail.com>
2024-03-02 15:22:42 +0900

    [refactor] global config 설정

---
+++ src/config/index.ts
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const GLOBAL_CONFIG = {
+  environment: process.env.NODE_ENV,
+  PUBLIC_URL: process.env.PUBLIC_URL ?? "http://localhost:8080",
+
+  REACT_APP_SERVICE_KEY: process.env.REACT_APP_SERVICE_KEY,
+
+  REACT_APP_MOVIE_DB_API_KEY: process.env.REACT_APP_MOVIE_DB_API_KEY,
+  REACT_APP_MOVIE_SERVICE_KEY: process.env.REACT_APP_MOVIE_SERVICE_KEY,
+  REACT_APP_MOVIE_DB_IMAGE_ROOT: process.env.REACT_APP_MOVIE_DB_IMAGE_ROOT,
+  REACT_APP_MOVIE_DB_API_ACCESS_TOKEN:
+    process.env.REACT_APP_MOVIE_DB_API_ACCESS_TOKEN,
+};
+export default GLOBAL_CONFIG;
